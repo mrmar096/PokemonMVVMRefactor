a3205b4a558e2bcd623054b64e03df77940890b1
mrmar <mario.munyoz@innocv.com>
2018-01-10 22:12:09 +0100

    Added Navigator layer

---
+++ app/src/main/java/net/azarquiel/pokemonappreal/infraestructure/Navigator.kt
@@ -0,0 +1,9 @@
+package net.azarquiel.pokemonappreal.infraestructure
+
+/**
+ * Created by mrmar on 10/1/18.
+ */
+interface Navigator {
+    fun startActivty()
+    fun showMessage()
+}
---
+++ app/src/main/java/net/azarquiel/pokemonappreal/infraestructure/NavigatorImpl.kt
@@ -0,0 +1,17 @@
+package net.azarquiel.pokemonappreal.infraestructure
+
+import android.content.Context
+
+/**
+ * Created by mrmar on 10/1/18.
+ */
+class NavigatorImpl(activityContext: Context) : Navigator {
+
+    override fun startActivty() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showMessage() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
