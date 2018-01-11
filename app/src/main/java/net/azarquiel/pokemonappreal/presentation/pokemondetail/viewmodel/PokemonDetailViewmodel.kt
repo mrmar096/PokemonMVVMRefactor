63b478799e9d01f8682594759c7a5a34f5b54f7c
mrmar <mario.munyoz@innocv.com>
2018-01-11 23:21:52 +0100

    Update viewmodel

--- app/src/main/java/net/azarquiel/pokemonappreal/presentation/pokemondetail/viewmodel/PokemonDetailViewmodel.kt
+++ app/src/main/java/net/azarquiel/pokemonappreal/presentation/pokemondetail/viewmodel/PokemonDetailViewmodel.kt
@@ -1,14 +1,42 @@
 package net.azarquiel.pokemonappreal.presentation.pokemondetail.viewmodel
 
 import android.databinding.BaseObservable
+import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
+import net.azarquiel.pokemonappreal.data.local.preferences.PokemonPreferences
+import net.azarquiel.pokemonappreal.domain.interactor.InteractorCallback
+import net.azarquiel.pokemonappreal.domain.interactor.local.PokemonInteractor
 import net.azarquiel.pokemonappreal.infraestructure.Navigator
-import net.azarquiel.pokemonappreal.presentation.pokemondetail.model.PokemonDetailModel
+import net.azarquiel.pokemonappreal.presentation.common.model.PokemonModel
 
 /**
  * Created by mrmar on 10/1/18.
  */
-class PokemonDetailViewmodel(navigator:Navigator):BaseObservable(){
-    val pokemonDetail : ObservableField<PokemonDetailModel> = ObservableField()
+class PokemonDetailViewmodel(val navigator:Navigator,
+                             val pokemonPreferences: PokemonPreferences,
+                             val pokemonInteractor: PokemonInteractor):BaseObservable(){
 
+    val pokemon: ObservableField<PokemonModel> = ObservableField(PokemonModel())
+
+
+    val isFavourite : ObservableBoolean = ObservableBoolean(false)
+
+
+    fun markAsFavourite(){
+        pokemonPreferences.saveFavourite(pokemon.get().id,pokemon.get().name)
+    }
+
+    fun fillPokemonView(id:Long){
+        pokemonInteractor.getPokemonById(id, object :InteractorCallback<PokemonModel>{
+            override fun success(data: PokemonModel?) {
+                pokemon.set(data)
+            }
+
+            override fun fail(t: Throwable) {
+                t.printStackTrace()
+            }
+
+        })
+        isFavourite.set(pokemonPreferences.isFavourite(id))
+    }
 }
